a1c6be3e542320588d5d6a7b9622b6393690d27c
Jean-Philippe André <jp@collimator.ai>
2022-08-17 16:25:01 +0900

    works in docker on cpu

--- single_test.py
+++ single_test.py
@@ -4,7 +4,7 @@ import random
 import logging
 from time import time
 
-import cv2
+from PIL import Image
 import numpy as np
 import torch
 
@@ -13,7 +13,8 @@ import torchvision.transforms as transforms
 from lib.models import PolyRegression
 
 def run_inference(image_path):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
 
     model_parameters = {
     "num_outputs": 35, # (5 lanes) * (1 conf + 2 (upper & lower) + 4 poly coeffs)
@@ -25,7 +26,7 @@ def run_inference(image_path):
     model = PolyRegression(**model_parameters)
     model.to(device)
 
-    loaded = torch.load("model.pt")
+    loaded = torch.load("model.pt", map_location=device)
     model.load_state_dict(loaded['model'])
     model.eval()
 
@@ -36,10 +37,10 @@ def infer(model, image_path: str, device = "cpu"):
 
     transform = transforms.ToTensor()
 
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    # image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image = Image.open(image_path)
     tensor = transform(image)
     tensor = tensor.unsqueeze(0)
-    print("image shape", image.shape)
     print("tensor shape", tensor.shape)
 
     with torch.no_grad():
