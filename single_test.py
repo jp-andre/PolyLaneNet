c6916faacacdbbb0c2502517ff74f4bf4535fe3a
Jean-Philippe André <jp@collimator.ai>
2022-09-06 16:00:08 +0900

    save:

--- single_test.py
+++ single_test.py
@@ -49,10 +49,13 @@ def load_model(device: str = None):
         'num_outputs': 35, # (5 lanes) * (1 conf + 2 (upper & lower) + 4 poly coeffs)
         'pretrained': True,
         'backbone': 'efficientnet-b0',
+        # 'backbone': 'resnet50',
         'pred_category': False,
         'curriculum_steps': [0, 0, 0, 0],
     }
     model_weights = 'model_tusimple_2695.pt'
+    # model_weights = 'model_tusimple_fulltrain_2695.pt'
+    # model_weights = 'model_tusimple_resnet50_2695.pt'
     model_location = fetch_model_weights(model_weights)
 
     model = PolyRegression(**model_parameters)
